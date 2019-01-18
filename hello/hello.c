0342a8ca678cde7b68883961d444cd39471efe03
ricBaba31 <46800923+ricBaba31@users.noreply.github.com>
2019-01-18 02:07:35 +0100

    Added hello.c file

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Riccardo
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Riccardo\n");
+
+  return 0;
+}
